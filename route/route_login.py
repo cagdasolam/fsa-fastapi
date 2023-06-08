893ff78761e254b2382b2aac1cb0b8927714f92a
Çağdaş Olam <44732969+cagdasolam@users.noreply.github.com>
2023-06-08 15:08:00 +0300

    remove expire date

--- route/route_login.py
+++ route/route_login.py
@@ -33,10 +33,10 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db:
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
         )
-    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    # access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.email}, expires_delta=access_token_expires
-    )
+        # data={"sub": user.email}, expires_delta=access_token_expires)
+        data={"sub": user.email})
     return {"access_token": access_token, "token_type": "bearer"}
 
 
